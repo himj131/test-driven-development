d07ce33a08b4c791231ba797256bc69cb0c6bb94
himj131@gmail.com <himj131@gmail.com>
2020-03-25 21:16:24 +0900

    유료서비스 만료일 계산 예제
    로직 구현 및 테스트 코드 리팩토링

--- src/main/java/chap03/ExpiryDateCalculator.kt
+++ src/main/java/chap03/ExpiryDateCalculator.kt
@@ -4,6 +4,6 @@ import java.time.LocalDate
 
 class ExpiryDateCalculator {
     fun calculateExpiryDate(billingDate: LocalDate, payAmount: Int): LocalDate {
-        return LocalDate.of(2019, 4, 1)
+        return billingDate.plusMonths(1)
     }
 }
--- src/test/java/chap03/ExpiryDateCalculatorTest.kt
+++ src/test/java/chap03/ExpiryDateCalculatorTest.kt
@@ -1,17 +1,29 @@
 package chap03
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
 internal class ExpiryDateCalculatorTest {
+    private lateinit var calculator: ExpiryDateCalculator
+
+    @BeforeEach
+    internal fun setUp() {
+        calculator = ExpiryDateCalculator()
+    }
+
     @Test
     internal fun 만원_납부하면_한달_뒤가_만료일임() {
-        val calculator = ExpiryDateCalculator()
-        var billingDate = LocalDate.of(2019,  3, 1)
-        val payAmount = 10000
+        assertExpiryDate(LocalDate.of(2019, 3, 1), 10000,
+                            LocalDate.of(2019, 4, 1))
 
-        var expiryDate: LocalDate = calculator.calculateExpiryDate(billingDate, payAmount)
-        assertEquals(LocalDate.of(2019, 4, 1), expiryDate)
+        assertExpiryDate(LocalDate.of(2019,  5, 1), 10000,
+                            LocalDate.of(2019, 6, 1))
+    }
+
+    private fun assertExpiryDate(billingDate: LocalDate, payAmount: Int, expectedExpiryDate: LocalDate) {
+        var realExpiryDate: LocalDate = calculator.calculateExpiryDate(billingDate, payAmount)
+        assertEquals(expectedExpiryDate, realExpiryDate)
     }
 }
