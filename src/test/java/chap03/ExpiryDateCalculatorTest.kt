82576c271d860eda435f130917b5c417da31660f
himj131@gmail.com <himj131@gmail.com>
2020-03-25 21:21:05 +0900

    유료서비스 만료일 계산
    예외 케이스 추가

--- src/test/java/chap03/ExpiryDateCalculatorTest.kt
+++ src/test/java/chap03/ExpiryDateCalculatorTest.kt
@@ -22,6 +22,16 @@ internal class ExpiryDateCalculatorTest {
                             LocalDate.of(2019, 6, 1))
     }
 
+    @Test
+    internal fun 납부일과_한달_뒤_일자가_같지_않음() {
+        assertExpiryDate(LocalDate.of(2019, 1, 31), 10000,
+                                LocalDate.of(2019, 2, 28))
+        assertExpiryDate(LocalDate.of(2019, 5, 31), 10000,
+                LocalDate.of(2019, 6, 30))
+        assertExpiryDate(LocalDate.of(2020, 1, 31), 10000,
+                LocalDate.of(2020, 2, 29))
+    }
+
     private fun assertExpiryDate(billingDate: LocalDate, payAmount: Int, expectedExpiryDate: LocalDate) {
         var realExpiryDate: LocalDate = calculator.calculateExpiryDate(billingDate, payAmount)
         assertEquals(expectedExpiryDate, realExpiryDate)
