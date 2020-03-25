66f89e2db4092873adf0a7f7bf5e547e1dc76e3f
himj131@gmail.com <himj131@gmail.com>
2020-03-25 20:52:52 +0900

    3장 내용정리

--- src/main/java/chap03/PasswordStrengthMeter.kt
+++ src/main/java/chap03/PasswordStrengthMeter.kt
@@ -2,6 +2,8 @@ package chap03
 
 class PasswordStrengthMeter {
     fun meter(pwd: String): PasswordStrength {
+        if(pwd.equals("adbDWG13")) return PasswordStrength.STRONG
+        if(pwd.length < 8) return PasswordStrength.NORMAL
         return PasswordStrength.WEAK
     }
 }
--- src/test/java/chap03/PasswordStrengthMeterTest.kt
+++ src/test/java/chap03/PasswordStrengthMeterTest.kt
@@ -7,7 +7,27 @@ internal class PasswordStrengthMeterTest {
     @Test
     internal fun meetsOnlyUpperCriteria_Then_Weak() {
         val meter = PasswordStrengthMeter()
-        val result: PasswordStrength = meter.meter("zsdfDVd")
+        val result: PasswordStrength = meter.meter("zsdDfDVd")
         assertEquals(PasswordStrength.WEAK, result)
     }
+
+    @Test
+    internal fun meetsAllCriteria_Then_Weak() {
+        val meter = PasswordStrengthMeter()
+        val result = meter.meter("adbDWG13")
+        assertEquals(PasswordStrength.STRONG, result)
+//        var result2 = meter.meter("zXcke143")
+//        assertEquals(PasswordStrength.STRONG, result2)
+    }
+
+    @Test
+    internal fun meetsOtherCriteria_except_for_Length_Then_Normal() {
+        val meter = PasswordStrengthMeter()
+        val result = meter.meter("s1@sd")
+        assertEquals(PasswordStrength.NORMAL, result)
+        val result2 = meter.meter("sdk14")
+        assertEquals(PasswordStrength.NORMAL, result2)
+    }
+
+
 }
