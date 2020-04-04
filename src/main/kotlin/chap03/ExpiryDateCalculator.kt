1178cd8f21390de0265406fc4c9fe8be430712f7
himj131@gmail.com <himj131@gmail.com>
2020-04-04 21:52:25 +0900

    리팩토링
    메소드 분리

--- src/main/kotlin/chap03/ExpiryDateCalculator.kt
+++ src/main/kotlin/chap03/ExpiryDateCalculator.kt
@@ -6,16 +6,24 @@ import java.time.YearMonth
 class ExpiryDateCalculator {
     fun calculateExpiryDate(payData: PayData): LocalDate {
         val addedMonths = payData.payAmount / 10000
-        payData.firstBillingDate?. let {
-            val candidateExp = payData.billingDate!!.plusMonths(addedMonths.toLong())
-
-            if(it.dayOfMonth != candidateExp.dayOfMonth) {
-                if(YearMonth.from(candidateExp).lengthOfMonth() < it.dayOfMonth) {
-                    return candidateExp.withDayOfMonth(YearMonth.from(candidateExp).lengthOfMonth())
-                }
-                return candidateExp.withDayOfMonth(it.dayOfMonth)
-            }
+        if(payData.firstBillingDate != null) {
+            return expiryDateUsingFirstBillingDate(payData, addedMonths)
+        } else {
+            return payData.billingDate!!.plusMonths(addedMonths.toLong())
         }
-        return payData.billingDate!!.plusMonths(addedMonths.toLong())
+    }
+
+    private fun expiryDateUsingFirstBillingDate(payData: PayData, addedMonths: Int): LocalDate {
+        val dayOfFirstBilling = payData.firstBillingDate!!.dayOfMonth
+        val candidateExp = payData.billingDate!!.plusMonths(addedMonths.toLong())
+
+        if(dayOfFirstBilling != candidateExp.dayOfMonth) {
+            val dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth()
+            if(dayLenOfCandiMon < dayOfFirstBilling) {
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon)
+            }
+            return candidateExp.withDayOfMonth(dayOfFirstBilling)
+        }
+        return candidateExp
     }
 }
